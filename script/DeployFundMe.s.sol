ebd8fb5d5ffcdb5ad7ae79458ead243f9a55f45e
Antoine James <mushow@proton.me>
2024-11-11 01:16:55 +0100

    feat: Add FundMe contract to deploy

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.28;
 
 import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
     function run() public {
         vm.startBroadcast();
+        new FundMe();
         vm.stopBroadcast();
     }
 }
