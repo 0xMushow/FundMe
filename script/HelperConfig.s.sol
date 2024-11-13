a86bbb6c565db11622d788e0d04ef23c7fa32cc9
Antoine James <mushow@proton.me>
2024-11-13 14:32:49 +0100

    draft: Add getAnvilConfig and refactored magic numbers

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,20 +2,27 @@
 
 pragma solidity ^0.8.28;
 
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
 
     NetworkConfig public activeNetworkConfig;
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    uint32 public constant SEP_CHAIN_ID = 11155111;
+    uint8 public constant ETH_CHAIN_ID = 1;
+
     struct NetworkConfig {
         address priceFeed;
     }
 
     constructor() {
-        if (block.chainid == 11155111) {
+        if (block.chainid == SEP_CHAIN_ID) {
             activeNetworkConfig = getSepoliaEthConfig();
-        } else if (block.chainid == 1) {
+        } else if (block.chainid == ETH_CHAIN_ID) {
             activeNetworkConfig = getMainnetEthConfig();
         } else {
             activeNetworkConfig = getAnvilConfig();
@@ -36,7 +43,18 @@ contract HelperConfig is Script {
         return mainnetConfig;
     }
 
-    function getAnvilConfig() public pure  returns (NetworkConfig memory) {
+    function getAnvilConfig() public returns (NetworkConfig memory) {
+        if(activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed : address(mockPriceFeed)
+        });
     }
 
 }
