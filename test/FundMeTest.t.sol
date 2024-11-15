fca9250f2a8e97bd9ad8ba0c187477211b1edecd
Antoine James <mushow@proton.me>
2024-11-15 20:42:53 +0100

    test: added more coverage

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,10 +8,12 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address user = makeAddr("antoine");
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(user, 100e18);
     }
 
     function testMinimumUsd() public view {
@@ -24,6 +26,35 @@ contract FundMeTest is Test {
 
     function testFund() public {
         fundMe.fund{value: 5e18}();
-        assertEq(fundMe.addressToAmountFunded(address(this)), 5e18);
+        assertEq(fundMe.getAddressToAmountFunded(address(this)), 5e18);
+    }
+
+    function testFundFailWithLessThanMinimum() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdateDataStructure() public {
+        fundMe.fund{value: 5e18}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(address(this));
+        assertEq(amountFunded, 5e18);
+    }
+
+    function testWithdrawFunds () public {
+        vm.prank(user);
+        fundMe.fund{value: 5e18}();
+        vm.prank(fundMe.i_owner());
+        fundMe.withdraw();
+        assertEq(fundMe.getAddressToAmountFunded(user), 0);
+    }
+
+    function getVersion() public view {
+        assertEq(fundMe.getVersion(), 4);
+    }
+
+    function testAddsFundersToArrayOfFunders() public {
+        vm.prank(user);
+        fundMe.fund{value: 5e18}();
+        assertEq(fundMe.getFunder(0), user);
     }
 }
